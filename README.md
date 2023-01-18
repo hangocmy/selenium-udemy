cd52d1bc9448a37b585f3d61c0e9da02e18e4248
hangocmy <hangocmy00@gmail.com>
2023-01-18 09:56:31 +0700

    python basic

---
+++ README.md
@@ -0,0 +1,4 @@
+# Install
+1. Install Python: http://python.org/download/
+2. Install pip: `python -m pip install -U pip`
+3. Install Selenium: `pip install Selenium` or `pip install -U selenium`
---
+++ StringDemo.py
@@ -0,0 +1,19 @@
+str = "HaNgocMyUdemy.com"
+str2 = "TDTU"
+str3 = "HaNgocMy"
+
+print(str[0:4]) #if you want substring in Python
+
+print(str+str2) #concatenation
+
+print(str3 in str) #substring check => True or False
+
+var = str.split(".") #split string by . => return list
+print (var)
+
+print(var[0])
+str4 = " great "
+print(str4.strip())
+print(str4.lstrip())
+
+print(str4.rstrip())
