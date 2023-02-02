601fc5377eb705700ea0d8a773360887439b58fd
hangocmy <hangocmy00@gmail.com>
2023-02-02 15:26:42 +0700

    code in section 9 - 16

--- pythonBasics/FunctionsDemo.py
+++ pythonBasics/FunctionsDemo.py
@@ -9,11 +9,43 @@ def GreetMe(name):
 
 def AddIntegers(a, b):
     return a+b
+dic = {"a": 2, 4:"bcd", "c": "Hello world"}
+
+# GreetMe("Rahul Shetty")
+#
+# print(AddIntegers(2, 3))
 
 
-GreetMe("Rahul Shetty")
+itemsInCart = 0
+#some operation
+if itemsInCart != 2:#    raise Exception("x should not exceed 7 ")
+    pass
 
-print(AddIntegers(2, 3))
+#assert(itemsInCart == 2)
+
+
+try:
+    with open('file.log') as file:
+        read_data = file.read()
+except:
+
+    print('Could not open file.log')
+
+
+try:
+    with open('test.txt', 'w') as file:
+        read_data = file.write("hello")
+except Exception as e:
+
+    print(e)
+
+finally:
+    print('Cleaning up, irrespective of any exceptions.')
+
+
+
+
+
 
 
 
