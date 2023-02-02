3e6d53c990e74bc1c7580c73d38d0c337f9cab33
hangocmy <hangocmy00@gmail.com>
2023-02-02 15:14:27 +0700

    exception

---
+++ pythonBasics/ExceptionsPython.py
@@ -0,0 +1,10 @@
+ItemInCart = 0
+
+if ItemInCart != 2:
+    raise Exception('Products Cart count not matching')
+
+#-=-=-=-=-=-=-=-=-
+
+# if ItemsInCart != 2:
+#     pass
+# assert (ItemInCart == 0)
