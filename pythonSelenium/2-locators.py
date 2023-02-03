5a3bd9fa91378e9cb3e9178779af25a366220b5c
hangocmy <hangocmy00@gmail.com>
2023-02-03 17:04:53 +0700

    update udemy

--- pythonSelenium/2-locators.py
+++ pythonSelenium/2-locators.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 
 #Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 
 service_obj = Service("C:\ChromeDriver\chromedriver.exe")
 driver = webdriver.Chrome(service=service_obj)
@@ -17,6 +18,14 @@ driver.find_element(By.ID, "exampleInputPassword1").send_keys("123456")
 driver.find_element(By.ID, "exampleCheck1").click()
 
 driver.find_element(By.CSS_SELECTOR, "input[name='name']").send_keys("Hello")
+driver.find_element(By.CSS_SELECTOR, "#inlineRadio2").click()
+
+#Static dropdown
+dropdown = Select(driver.find_element(By.ID, "exampleFormControlSelect1"))
+dropdown.select_by_visible_text("Female")
+dropdown.select_by_index(0)
+#dropdown.select_by_value()
+
 driver.find_element(By.XPATH, "//input[@type='submit']").click()
 
 message = driver.find_element(By.CLASS_NAME, "alert-success").text
