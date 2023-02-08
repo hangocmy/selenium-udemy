4992d7613c40f7239529c07a4a23393a33c03de2
hangocmy <hangocmy00@gmail.com>
2023-02-08 17:02:35 +0700

    add

--- B-pythonSelenium/17-e2eTest.py
+++ B-pythonSelenium/17-e2eTest.py
@@ -15,7 +15,7 @@ driver.get("https://rahulshettyacademy.com/angularpractice/")
 driver.find_element(By.CSS_SELECTOR," a[href*='shop']").click()
 products = driver.find_elements(By.XPATH,"//div[@class='card h-100']")
 
-for product in products :
+for product in products:
     productName = product.find_element(By.XPATH, "div/h4/a").text
     if productName == "Blackberry":
         product.find_element(By.XPATH, "div/button").click()
@@ -31,6 +31,8 @@ driver.find_element(By.CSS_SELECTOR,"[type='submit']").click()
 successText = driver.find_element(By.CLASS_NAME,"alert-success").text
 assert "Success! Thank you!" in successText
 driver.close()
+
+
 
 
 
